5b8ff687851fdc53b02b5087cc2ea872d43d3f0d
Peter Clark <pjclark@email.sc.edu>
2017-01-04 16:58:36 -0500

    Index Of Coincidence Implementation
    Still needs a little work, but right now it runs through an assortment
    and performs the computations to figure out the IoC of each one. It
    returns the average of the IoCs which may or may not be satisfactory

--- CryptographyLibrary/CryptographyLibrary/Program.cs
+++ CryptographyLibrary/CryptographyLibrary/Program.cs
@@ -6,10 +6,24 @@ namespace CryptographyLibrary
 	{
 		public static void Main (string[] args)
 		{
-			string alphabet = "abcdefghiklmnopqrstuvwxyz";
+            /*string alphabet = "abcdefghiklmnopqrstuvwxyz";
 			string key = "zombie";
 
 			ADFGX adfgx = new ADFGX(alphabet, key);
-		}
+            */
+
+            string ciphertext = "Aazek haafx, eh bo o ckzeu uhkr wwm, keuapzr oc?";
+            int maxKeyLength = 3;
+
+            for (int i = 1; i <= maxKeyLength; ++i)
+            {
+                double ioc = Utilities.CalculateIOC(ciphertext, i);
+
+                Console.WriteLine("Key size: " + i);
+                Console.WriteLine(ioc);
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
 	}
 }
---
+++ CryptographyLibrary/CryptographyLibrary/Utilities.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptographyLibrary
+{
+    class Utilities
+    {
+        public static double CalculateIOC(string text, int keyLength)
+        {
+            text = RemoveSpecialCharacters(text).ToLower();
+
+            //double[] ioc = new double[keyLength];
+            string[] sepText = new string[keyLength];
+            double value = 0.0;
+
+            for(int i = 0; i < text.Length; ++i)
+            {
+                sepText[i % keyLength] += text[i];
+            }
+
+            for(int i = 0; i < sepText.Length; ++i)
+            {
+                value += IndexOfCoincidence(sepText[i]);
+            }
+            value /= sepText.Length;
+
+            return value;
+        }
+
+        public static double IndexOfCoincidence(string text)
+        {
+            text = RemoveSpecialCharacters(text).ToLower();
+
+            double ioc = 0.0;
+            double sum = 0.0;
+            double div = text.Length * (text.Length - 1);
+            string alphabet = new String(text.Distinct().ToArray());
+
+            foreach (var c in alphabet)
+            {
+                int freq = 0;
+                for (int index = text.IndexOf(c); index >= 0; index = text.IndexOf(c, index + 1))
+                {
+                    ++freq;
+                }
+
+                sum += freq * (freq - 1);
+            }
+
+            ioc = sum / div;
+
+            return ioc;
+        }
+
+        //http://stackoverflow.com/questions/1120198/most-efficient-way-to-remove-special-characters-from-string
+        public static string RemoveSpecialCharacters(string str)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in str)
+            {
+                if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '.' || c == '_')
+                {
+                    sb.Append(c);
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
